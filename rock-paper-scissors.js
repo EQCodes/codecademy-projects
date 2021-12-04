40f969c861a8f9546a09effdd29050e62a3eac0e
EQCodes <eilidh.mcq@gmail.com>
2021-12-04 17:31:02 +0000

    rock paper scissors complete

---
+++ rock-paper-scissors.js
@@ -0,0 +1,62 @@
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
+    return userInput;
+    } else {
+      console.log('Please enter rock, paper or scissors.');
+    }
+  };
+  
+  function getComputerChoice () {
+    const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+      case 0:
+        return 'rock';
+      case 1:
+        return 'paper';
+      case 2:
+        return 'scissors';
+    }
+  };
+  
+  function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+      return 'The game is a tie!';
+    }
+    if (userChoice === 'rock') {
+      if (computerChoice === 'paper') {
+      return 'The computer won!';
+    } else {
+      return 'The user won!'; 
+    }
+  }
+  if (userChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return 'The computer won!';
+    } else {
+      return 'The user won!';
+    }
+  }
+  
+  if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      return 'The computer won!';
+    } else {
+      return 'The user won!';
+    }
+  }
+  if (userChoice === 'bomb') {
+    return 'YOU WON!';
+  }
+  }
+  
+  const playGame = () => {
+    const userChoice = getUserChoice('paper');
+    const computerChoice = getComputerChoice();
+    console.log('You threw: ' + userChoice);
+    console.log('The computer threw: ' + computerChoice);
+    console.log(determineWinner(userChoice, computerChoice))
+  }
+  
+  playGame();
+  
